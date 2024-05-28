36176fd8962e0d11965b96fb27a07a1227820ce1
sabermodirian <sabermodirian@gmail.com>
2024-05-28 23:38:48 +0330

    Update views.py
    "HELLO First HTTP_TEST that created by SABER"

--- mysite/views.py
+++ mysite/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def http_test(request):
-    return HttpResponse("HELLO First HTTP_TEST that created by SABER")
+    return HttpResponse('<h1>"HELLO First HTTP_TEST that created by SABER"</h1>')
