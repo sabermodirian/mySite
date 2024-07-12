c03bf83185485b94b6bcbf6dde8912d50ad2589b
sabermodirian <sabermodirian@gmail.com>
2024-07-12 13:32:57 +0330

    debug string
    debuging toolong strins in views.py and settings.py and manage.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse , JsonResponse
+# from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 
 def http_test(request):
     return HttpResponse('<h1>"HELLO First HTTP_TEST created by SABER"</h1>')
 
+
 def json_test(request):
-    return JsonResponse({'name' : 'Saber SASA'})
+    return JsonResponse({'name': 'Saber SASA'})
