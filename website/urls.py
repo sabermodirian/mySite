af463235577d5675cf9299851656bc4b3e40acba
sabermodirian <sabermodirian@gmail.com>
2024-08-09 11:08:36 +0330

    create temlate html page
    Creat e a base template html file base.html and extends (INHERITANCE) all html pages from base

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,6 @@ from website.views import *
 urlpatterns = [
     path('', index_view),
     path('about',about_view),
-    path('contact',contact_view)
+    path('contact',contact_view),
+    #path('base',base_view),
     ]
--- website/views.py
+++ website/views.py
@@ -13,4 +13,6 @@ def about_view(request):
 def contact_view(request):
     return render(request, 'website\contact.html')
 
+    
+
 
