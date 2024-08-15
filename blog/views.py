57552eada31f55742b500a937dccc03dc6612b09
sabermodirian <sabermodirian@gmail.com>
2024-08-15 11:55:45 +0330

    template tags
    create atest .html and use template tags like :
    {FOR ENDFOR}  and
    {IF ENDIF}
    in this test.html
    and retriev data frm DB and shows in test.htlm page.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from blog.models import post
+
 
 def blog_view(request):
     return render(request, 'blog/blog-home.html')
 def blog_single(request):
     return render(request, 'blog/blog-single.html')
+
+def test(request):
+    posts = post.objects.all()
+    context = {'posts':posts}
+    return render(request,'test.html',context)
