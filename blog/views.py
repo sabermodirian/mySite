bb2390a11397e38ba4a1c66d474ac4c278613186
sabermodirian <sabermodirian@gmail.com>
2024-08-15 21:56:43 +0330

    show data model in site template
    show data model in site template :
    -retreive data from DB and showing in main  site template blog- Home

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,10 @@ from blog.models import post
 
 
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = post.objects.filter(status=1)
+    contex = {'posts':posts}
+    return render(request, 'blog/blog-home.html',contex)
+
 def blog_single(request):
     return render(request, 'blog/blog-single.html')
 
