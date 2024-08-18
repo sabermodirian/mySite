a90b88bbd54dfe48efeda21c56ff7a05da71ba87
sabermodirian <sabermodirian@gmail.com>
2024-08-18 19:32:40 +0330

    blog اضافه کردن فیلد تصاویر برای
    Adding ImageField for blog and shwing image  special post in Blog-Home.html

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class post(models.Model):
-    #image
+    image = models.ImageField(upload_to = 'blog/',default='blog/default.jpg')
     #author = models.ForeignKey(User, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
