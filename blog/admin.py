34718c3e0ffdeb5a1403737300d9d1073aa8d148
sabermodirian <sabermodirian@gmail.com>
2024-08-19 12:26:47 +0330

    اضافه کردن فیلد دسته بندی Category
    Adding Category Field  to blog for categorize Tags related to the post and Adding Tag Items to  posts  by Admin.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import post
+from blog.models import post, Category
 
 
 # Register your models here.
@@ -15,3 +15,4 @@ class postAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content')
     
 admin.site.register(post , postAdmin)
+admin.site.register(Category)
