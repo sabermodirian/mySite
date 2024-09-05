7616ee3e416e47c1572743bad149e68de1e25276
sabermodirian <sabermodirian@gmail.com>
2024-09-05 21:05:42 +0330

    part01 for forms .form
    بدون اصلاحیه همراه با کدهای کامنت شده

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from website.models import contact
+from website.forms import NameForm
 
 
 def index_view(request):
@@ -14,24 +15,48 @@ def about_view(request):
 def contact_view(request):
     return render(request, 'website\contact.html')
 
-from django.views.decorators.csrf import csrf_protect
+# from django.views.decorators.csrf import csrf_protect
 
-@csrf_protect
+# @csrf_protect
 def test_view(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
-        c = contact()
-        c.name = name
-        c.email = email
-        c.subject = subject
-        c.message = message
-        c.save()
-        print(name,email,subject,message)
-    elif request.method == 'GET': 
-        print('GET')   
-    return render(request,'test.html',{})    
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return HttpResponse('Form submitted successfully')
+        else:
+            return HttpResponse('Form is not valid')
+    form = NameForm() 
+    return render(request, 'test.html', {'form': form})  
+
+
+
+     
+            
+                # name = form.cleaned_data['name']
+                # email = form.cleaned_data['email']
+                # subject = form.cleaned_data['subject']
+                # message = form.cleaned_data['message']
+                # c = contact()
+                # c.name = name
+                # c.email = email
+                # c.subject = subject
+                # c.message = message
+                # c.save()
+                # print(name,email,subject,message)
+                # return JsonResponse({'message': 'Form submitted successfully'})
+    #     name = request.POST.get('name')
+    #     email = request.POST.get('email')
+    #     subject = request.POST.get('subject')
+    #     message = request.POST.get('message')
+    #     c = contact()
+    #     c.name = name
+    #     c.email = email
+    #     c.subject = subject
+    #     c.message = message
+    #     c.save()
+    #     print(name,email,subject,message)
+    # elif request.method == 'GET': 
+    #     print('GET')   
+    # return render(request,'test.html',{})    
 
 
