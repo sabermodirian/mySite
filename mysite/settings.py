ccdf07edc4ccb00e6987561cc23fe6aec2976283
sabermodirian <sabermodirian@gmail.com>
2024-09-12 19:58:22 +0330

    نصب ماژولهای Humanizeو Django-etensions: Install Humanize and Django-extensions Modules
    This commit adds two essential modules to the project: Humanize and Django-extensions. Below is an explanation of each module and its purpose within the project:
    
    Humanize Module: The Humanize module provides several template filters in Django that allow for more readable formatting of numbers, dates, and other common data types. For example, it can turn numbers into “human-readable” formats like 1,000 as “1k” or dates like “3 days ago”. This is particularly useful when presenting data to end-users, making it more intuitive and easier to interpret.
    
    Benefits include:
    
    Better UX through improved formatting for end-users.
    Can be applied in various places like financial data, reports, and timestamps.
    Works out-of-the-box with Django templates.
    Django-extensions Module: The Django-extensions module extends Django’s default features with many helpful utilities that simplify the development process. It includes management commands like shell_plus, graph_models, and runserver_plus, which boost productivity, especially during the development and debugging stages.
    
    Benefits include:
    
    A more powerful shell_plus command that automatically imports all models.
    The ability to visualize database models using graph_models.
    Enhances the default development server with runserver_plus, providing better debugging and error reporting.

--- mysite/settings.py
+++ mysite/settings.py
@@ -60,6 +60,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
 
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'django_extensions',
+
     
     'website.apps.WebsiteConfig',
     
